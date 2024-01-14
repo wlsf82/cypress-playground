592413b985bfb5dde76ff66ae61ddbf455e385c3
Walmyr <wlsf82@gmail.com>
2024-01-14 04:49:10 +0100

    Add the next failing test

--- cypress/e2e/playground.cy.js
+++ cypress/e2e/playground.cy.js
@@ -7,10 +7,20 @@ describe('Cypress Playground - Test Design Masterclas TAT', () => {
   })
 
   it('clicks a button and ensure an action is triggered', () => {
+    cy.clock()
     cy.contains('#click button', 'Subscribe').click()
     cy.contains(
-      '#click span',
-      "You've been successuflly subscribed to our newletter."
+      '#click span#success',
+      "You've been successfully subscribed to our newsletter.",
     ).should('be.visible')
+    cy.tick(3000)
+    cy.contains(
+      '#click span#success',
+      "You've been successfully subscribed to our newsletter.",
+    ).should('not.be.visible')
+  })
+
+  it('types in an input which "signs" a form', () => {
+    cy.get('#type textarea').type('Hi, Cypress Test Design Masterclass!')
   })
 })
