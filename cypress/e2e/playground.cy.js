9642d85f3b819a4d3ee42fad0a38d44813dfe229
Walmyr <wlsf82@gmail.com>
2024-01-14 03:58:06 +0100

    Write the next failing test

--- cypress/e2e/playground.cy.js
+++ cypress/e2e/playground.cy.js
@@ -1,8 +1,12 @@
 describe('Cypress Playground - Test Design Masterclas TAT', () => {
-  it('visits the page and assert a expected text is visible', () => {
-    cy.visit('./index.html')
+  beforeEach(() => cy.visit('./index.html'))
 
+  it('after visiting a page, asserts some text is visible', () => {
     cy.contains('h1', 'Cypress Playground').should('be.visible')
     cy.contains('#visit h2', 'cy.visit()').should('be.visible')
+  })
+
+  it('clicks a button and ensure an action is triggered', () => {
+    cy.contains('#click button', 'Subscribe.').click()
   })
 })
