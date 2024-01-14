ac0492afcd2f3688a957a77f9f4cd08d1da8c007
Walmyr <wlsf82@gmail.com>
2024-01-14 03:53:27 +0100

    Make the first test pass

--- cypress/e2e/playground.cy.js
+++ cypress/e2e/playground.cy.js
@@ -2,7 +2,7 @@ describe('Cypress Playground - Test Design Masterclas TAT', () => {
   it('visits the page and assert a expected text is visible', () => {
     cy.visit('./index.html')
 
-    cy.contains('h1', 'Foo').should('be.visible')
-    cy.contains('h2', 'bar').should('be.visible')
+    cy.contains('h1', 'Cypress Playground').should('be.visible')
+    cy.contains('#visit h2', 'cy.visit()').should('be.visible')
   })
 })
