a66811b873625da1f3e57dc2370227807c919e57
Walmyr <wlsf82@gmail.com>
2024-01-16 17:47:59 +0100

    Shorten test suite description

--- cypress/e2e/playground.cy.js
+++ cypress/e2e/playground.cy.js
@@ -1,4 +1,4 @@
-describe('Cypress Playground - Test Design Masterclas TAT', () => {
+describe('Cypress Playground', () => {
   beforeEach(() => {
     if (Cypress.env('environment') === 'prod') {
       cy.visit('https://cypress-playground.s3.eu-central-1.amazonaws.com/index.html')
