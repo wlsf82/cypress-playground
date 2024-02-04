ee27ca4524dcefe333f70d3f021a3ac57288d0ce
Walmyr <wlsf82@gmail.com>
2024-02-04 19:16:53 +0100

    Add test to simulate API failure

--- cypress/e2e/playground.cy.js
+++ cypress/e2e/playground.cy.js
@@ -174,6 +174,36 @@ describe('Cypress Playground', () => {
     cy.contains('#intercept ul li', 'User ID: ').should('be.visible')
   })
 
+  it('simulates the server/API going "down" then back "up"', () => {
+    cy.intercept(
+      'GET',
+      'https://jsonplaceholder.typicode.com/todos/1',
+      { statusCode: 500 }
+      ).as('serverError')
+    cy.contains('#intercept button', 'Get TODO').click()
+    cy.wait('@serverError')
+      .its('response.statusCode')
+      .should('be.equal', 500)
+    cy.contains(
+      '#intercept .error span',
+      'Oops, something went wrong. Refresh the page and try again.'
+    ).should('be.visible')
+    cy.reload()
+    cy.intercept(
+      'GET',
+      'https://jsonplaceholder.typicode.com/todos/1',
+      { middleware: true }
+    ).as('getTodo')
+    cy.contains('#intercept button', 'Get TODO').click()
+    cy.wait('@getTodo')
+      .its('response.statusCode')
+      .should('be.equal', 200)
+    cy.contains('#intercept ul li', 'TODO ID: ').should('be.visible')
+    cy.contains('#intercept ul li', 'Title: ').should('be.visible')
+    cy.contains('#intercept ul li', 'Completed: ').should('be.visible')
+    cy.contains('#intercept ul li', 'User ID: ').should('be.visible')
+  })
+
   it('makes an HTTP request and asserts on the returned status code', () => {
     cy.request('GET', 'https://jsonplaceholder.typicode.com/todos/1')
       .its('status')
